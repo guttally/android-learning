0767410a942a7d95bd0d2a2febbde4230869e647
guttally <guttally@gmail.com>
2018-09-15 00:16:42 +0900

    Add App class

---
+++ dagger-viewmodel/app/src/main/java/net/guttally/dagger_viewmodel/App.java
@@ -0,0 +1,10 @@
+package net.guttally.dagger_viewmodel;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
